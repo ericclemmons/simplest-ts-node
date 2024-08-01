0b14c5e3ead3e0551cb99c216d40c626773d664d
Eric Clemmons <eric@smarterspam.com>
2024-07-31 21:51:30 -0500

    dev + test

---
+++ src/add.test.ts
@@ -0,0 +1,8 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+describe("math", () => {
+  it("should still be mathing", () => {
+    assert.equal(5, 2 + 3);
+  });
+});
